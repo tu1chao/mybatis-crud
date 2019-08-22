f65d4cece7bb5abb4c00f170bc2bb05d275007af
lenovo <lenovo@DESKTOP-DOBF7UR>
2019-08-22 17:53:14 +0800

    mybatis-crud

---
+++ src/com/how2java/TestMybatis.java
@@ -0,0 +1,64 @@
+package com.how2java;
+ 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+ 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+ 
+import com.how2java.pojo.Category;
+ 
+public class TestMybatis {
+ 
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+ 
+        listAll(session);
+
+        session.commit();
+        session.close();
+ 
+    }
+
+	private static void update(SqlSession session) {
+		Category c= session.selectOne("getCategory",3);
+        c.setName("修改了的Category名稱");
+        session.update("updateCategory",c);
+         
+        listAll(session);
+	}
+
+	private static void get(SqlSession session) {
+		Category c= session.selectOne("getCategory",3);
+        
+        System.out.println(c.getName());
+	}
+
+	private static void delete(SqlSession session) {
+		Category c = new Category();
+        c.setId(6);
+        session.delete("deleteCategory",c);
+        listAll(session);
+	}
+
+	private static void add(SqlSession session) {
+		Category c = new Category();
+        c.setName("新增加的Category");
+        session.insert("addCategory",c);
+         
+        listAll(session);
+	}
+ 
+    private static void listAll(SqlSession session) {
+        List<Category> cs = session.selectList("listCategory");
+        for (Category c : cs) {
+            System.out.println(c.getName());
+        }
+    }
+}
